45f6c3f4cbc54341c5bf15c52593712481d29a24
Fatma Hassan <hfatma791@gmail.com>
2023-01-06 19:29:04 +0200

    Server Connection

--- src/serve/tic/tac/toe/Server.java
+++ src/serve/tic/tac/toe/Server.java
@@ -5,7 +5,40 @@
  */
 package serve.tic.tac.toe;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Vector;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 
 public class Server extends Thread{
+    ServerSocket serverSocket;
+    public static volatile DatabaseOperations operations;
+    public static volatile Vector<MessageHandler>myClients=new Vector<MessageHandler>();
     
+    public Server(){
+          operations=new DatabaseOperations();
+        try {
+            serverSocket=new ServerSocket(5005);
+        } catch (IOException ex) {
+            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    
+    }
+
+    @Override
+    public void run() {
+     
+        while(true)
+        {
+            try {
+               Socket socket=serverSocket.accept();
+               Server.myClients.add( new MessageHandler(socket));//here set status true
+            } catch (IOException ex) {
+                Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
 }
