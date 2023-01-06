ad9776f4cf2e1cde172d47f8a4ace1eb6bc7796b
Yasmeen Ataa <yasmenataa7@gmail.com>
2023-01-06 19:03:41 +0200

    Update Database Class

--- src/serve/tic/tac/toe/Database.java
+++ src/serve/tic/tac/toe/Database.java
@@ -1,11 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package serve.tic.tac.toe;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import org.apache.derby.jdbc.ClientDriver;
 
 
 public class Database {
+    private Connection con;
+    private PreparedStatement statement;
+    
+    public Database(){
+      
+        try {
+            DriverManager.registerDriver(new ClientDriver());
+            con=DriverManager.getConnection("jdbc:derby://localhost:1527/Tic-Tac-Toe", "root","root");
+           
+        } catch (SQLException ex) {
+           ex.printStackTrace();
+        }
+    }
     
 }
